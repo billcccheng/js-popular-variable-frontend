6d1e816b200580921070ebdc1f6482496c3849c6
Bill <iamkg05@gmail.com>
2018-02-03 23:02:26 -0600

    fixed typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,10 +18,10 @@ class Home extends Component {
         There was this day when I was code reviewing one of the tickets that was assigned to me. It was a huge ticket and lots of code refactoring. Along the way, I saw this function named <i>checkNextBillingDate</i>. At first I assumed this meant returning back a string or date object. However, diving deeper into the code I found out that variable was returning back a boolean value. WHAT?!?! How does <i>checkNextBillingDate</i> return a boolean? Even if it did, how would I know what it was checking. Well after digging into the function, I finally figured out it was trying to check to check if the date passed in was a future. A better naming would have maybe been <i>isDateInFuture</i> or something like that.
         </p>
         <p>
-        The last reason and the most important reason: Since I am not a native English speaker and believe me...naming variable is painful for non native speaker since we don't have the vocabulary size as a native speaker. During my undergraduate years in Taiwan, I had a lot of friends who were naming their variables in all sorts of strange name or to make matters worse: they had invented or misspelled a variable name they thought was something. Reading those code was a nightmare, not saying code reviewing them.
+        The last reason and the most important reason: Since I am not a native English speaker and believe me...naming variable is painful for non native speaker since we don't have the vocabulary size as a native speaker. During my undergraduate years in Taiwan, I had a lot of friends who were naming their variables in all sorts of strange name. To make matters worse: they had invented or misspelled a variable name they thought was something. Reading those code was a nightmare, not saying code reviewing them.
         </p>
         <p>
-          To sum it up, I wish the app can be useful for people to reference how popoular prepos name their variables or functions. In the end, all good software developer learn from the best codes and I believe that the most popular repos are always a good place to start.
+          To sum it up, I wish the app can be useful for people to reference how popoular repos name their variables or functions. In the end, all good software developer learn from the best codes and I believe that the most popular repos are always a good place to start.
         </p>
         Bill Cheng
       </div>);
